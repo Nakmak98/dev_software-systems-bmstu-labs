cd0a1ff9396452635ec7644745457cc24589de2c
Nakmak98 <Nakmak1998@gmail.com>
2019-05-22 01:17:48 +0300

    first version

--- GaussMultithread.c
+++ GaussMultithread.c
@@ -1,55 +1,29 @@
 /*
- * Программа предназначена для решения СЛАУ методом Гаусса.
+ * Библиотека предназначена для решения СЛАУ методом Гаусса.
  * Прямой ход метода Гаусса выполняют параллельные потоки,
  * которые ведут исключение элементов в прямом ходе по столбцам.
  *
- * Программа, также, считает чистое время решения уравнения,
- * исключая накладные расходы.
- *
- * Компиляция: сс GaussMultithread.c -lptread -o gauss
- * Запуск: ./gauss <количество потоков>
+ * Компиляция: сс <ваша прикладная программа> GaussMultithread.c -lpthread
+ * ВАЖНО: подключите заголовочный файл GaussMultithread.h
  */
-#include <stdlib.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <time.h>
-#include <sys/time.h>
 
-#define N 2048 //размерность матрицы
+#include "GaussMultithread.h"
 
 /*
- * Структура предназначена для хранения
- * необходимой информации для синхронизации потоков
- */
-typedef struct {
-    pthread_t tid; // идентификатор потока
-    int start;     // начальная строка для исключения
-    int step;      // шаг для определения следующей строки
-} Threads;
-
-void *thread_exclusion(void *arg_p);
-
-void matrix_initialization();
-
-void direct_move();
-
-void reverse_move();
-
-void print_matrix();
-
-int time_stop();
-
-struct timeval timeval1, timeval2, diff_timeval;
-struct timezone tz;
-
-pthread_barrier_t bar1;
-pthread_barrier_t bar2;
-
-double *A, *B, *X;
-
-int main(int argc, char **argv) {
-    int i;
-    int threads_num = atoi(argv[1]);
+ * Функция-решатель. Создает потоки управления (4), которые ведут исключение элементов по столбцам
+ * Поток main ответственен за выбор ведущей строки и столбца.
+ * */
+int gauss_solve(const double *A, double *X, const double *B, int n, int m) {
+    /*
+     *  const double *A - матрица коэффициентов (в виде одномерного массива)
+     *  double *B       - вектор свободных членов
+     *  const double *X - вектор неизвестных.
+     *  n - количество неизвестных
+     *  m - количество уравнений
+     * */
+    const int threads_num = 4;
+    N = n;
+    M = m;
     pthread_attr_t pattr;
     pthread_attr_init(&pattr);
     pthread_attr_setscope(&pattr, PTHREAD_SCOPE_SYSTEM);
@@ -57,32 +31,26 @@ int main(int argc, char **argv) {
     pthread_barrier_init(&bar1, NULL, threads_num + 1);
     pthread_barrier_init(&bar2, NULL, threads_num + 1);
     Threads *threads = (Threads *) calloc(threads_num, sizeof(Threads));
-    A = (double *) calloc(N * N, sizeof(double));
-    X = (double *) calloc(N, sizeof(double));
-    B = (double *) calloc(N, sizeof(double));
-    matrix_initialization();
-    gettimeofday(&timeval1, &tz);
-    for (i = 0; i < threads_num; i++) {
+    gauss_A = (double *) calloc(N * M, sizeof(double));
+    gauss_B = (double *) calloc(N, sizeof(double));
+    matrix_initialization(A, B);
+    for (int i = 0; i < threads_num; i++) {
         threads[i].start = i;
         threads[i].step = threads_num;
         if (pthread_create(&threads[i].tid, &pattr, &thread_exclusion, &threads[i]))
-            perror("pthread_create");
+            perror("Error: pthread_create() return exit status;");
     }
     direct_move();
-    reverse_move();
-    for (i = N - 20; i < N; i++) {
-        printf("%f\n", X[i]);
-    }
-    printf("Time: %d ms\n", time_stop());
-    free(X);
-    free(A);
-    free(B);
+    reverse_move(X);
+    free(gauss_A);
+    free(gauss_B);
     free(threads);
     pthread_barrier_destroy(&bar1);
     pthread_barrier_destroy(&bar2);
     pthread_attr_destroy(&pattr);
     return 0;
 }
+
 
 /*
  * Функция выполняет нормирование ведущей строки
@@ -98,11 +66,11 @@ void direct_move() {
     int i, j;
     double temp;
     for (i = 0; i < N; i++) {
-        temp = A[N * i + i];
+        temp = gauss_A[N * i + i];
         for (j = i; j < N; j++) {
-            A[N * i + j] /= temp;
+            gauss_A[N * i + j] /= temp;
         }
-        B[i] /= temp;
+        gauss_B[i] /= temp;
         pthread_barrier_wait(&bar1);
         pthread_barrier_wait(&bar2);
     }
@@ -110,67 +78,60 @@ void direct_move() {
 
 /*
  * Выполнение обратного хода Гаусса
- * Функция изменяет внешний массив X
+ * 1*X[n] = B[n]                            | 1  a22 | * | x1 | = | b1 |
+ * 1*X[n-1] + a[n-1][n]*X[n] = B[n-1]       | 0   1  | * | x2 | = | b2 |
+ * X[n-1] = B[n-1] - X[n]*a[n-1][n]*X[n]
+ * ...
  */
-void reverse_move() {
+void reverse_move(double *X) {
+    // double *X - вектор неизвестных
     int i = 0, j = 0;
     for (i = N - 1; i >= 0; i--) {
-        X[i] = B[i];
+        X[i] = gauss_B[i];
         for (j = N - 1; j > i; j--) {
-            X[i] -= X[j] * A[i * N + j];
+            X[i] -= X[j] * gauss_A[i * N + j];
         }
     }
 }
 
 /*
- * Инициализирует внешний массив A случайными значениями от -1 до 1.
- * Элементы внешнего массива B есть сумма элементов строки матрицы А.
+ * Копирование матрицы A и B во внутренние массивы
  */
-void matrix_initialization() {
+void matrix_initialization(const double *A, const double *B) {
     int i, j;
-    srand(time(NULL));
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
-            A[N * i + j] = ((double) rand() * 2 / RAND_MAX) - 1.0;
-            B[i] += A[N * i + j];
+            gauss_A[N * i + j] = A[N * i + j];;
+            gauss_B[i] = B[i];
         }
     }
-}
-
-// Функция выводит матрицу A и вектор B
-void print_matrix() {
-    int i, j;
-    printf("Matrix A: \n");
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++)
-            printf("%f ", A[N * i + j]);
-        putchar('\n');
-    }
-    printf("\nMatrix B: \n");
-    for (i = 0; i < N; i++) {
-        printf("%f\n", B[i]);
-    }
-    putchar('\n');
 }
 
 /*
  * Функция которую выполняют потоки для исключения элементов
  * матрицы свободных коэффициентов по столбцам.
+ * Распараллеливание ведется следующим образом:
+ *
+ *  thread1| 1 a11 a12 | b1 |
+ *  thread2| 0 a21 a22 | b2 |
+ *  thread1| 0 a31 a32 | b3 |
  */
 void *thread_exclusion(void *threads) {
-    int i, j, k;
     double temp;
     Threads *thread = (Threads *) threads;
-    for (j = 0; j < N; j++) {
-        if (j >= N - 1)
+    for (int j = 0; j < N; j++) {
+        if (j >= N - 1) // условие остановки потоков
             break;
         pthread_barrier_wait(&bar1); //поток ожидает, когда main закончит работу с ведущей строкой
-        for (i = j + 1 + thread->start; i < N; i += thread->step) { // определение строки, которую обрабатывает поток
-            temp = A[N * i + j];
-            B[i] = (B[i] / temp) - B[j];
-            for (k = j; k < N; k++) {
-                A[N * i + k] /= temp;
-                A[N * i + k] -= A[N * j + k];
+
+        for (int i = j + 1 + thread->start; i < N; i += thread->step) { // определение строки, которую обрабатывает поток
+            temp = gauss_A[N * i + j];
+            if (temp == 0) // если элемент уже нулевой,
+                continue;  // то не производить исключение
+            gauss_B[i] = (gauss_B[i] / temp) - gauss_B[j];
+            for (int k = j; k < N; k++) {
+                gauss_A[N * i + k] /= temp;
+                gauss_A[N * i + k] -= gauss_A[N * j + k];
             }
         }
         pthread_barrier_wait(&bar2); // поток закончил обрабатывать столбец
@@ -179,18 +140,4 @@ void *thread_exclusion(void *threads) {
     pthread_barrier_wait(&bar1);
     pthread_barrier_wait(&bar2);
     pthread_exit(0);
-}
-
-/*
- * Функция считает чистое времени решения уравнения
- */
-int time_stop() {
-    gettimeofday(&timeval2, &tz);
-    diff_timeval.tv_sec = timeval2.tv_sec - timeval1.tv_sec;
-    diff_timeval.tv_usec = timeval2.tv_usec - timeval1.tv_usec;
-    if (diff_timeval.tv_usec < 0) {
-        diff_timeval.tv_sec--;
-        diff_timeval.tv_usec += 1000000;
-    }
-    return diff_timeval.tv_sec * 1000 + diff_timeval.tv_usec / 1000;
 }
---
+++ GaussMultithread.h
@@ -0,0 +1,31 @@
+#ifndef MKR_GAUSSMULTITHREAD_H
+#define MKR_GAUSSMULTITHREAD_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+/*
+ * Структура предназначена для хранения
+ * необходимой информации для синхронизации потоков
+ */
+typedef struct {
+    pthread_t tid; // идентификатор потока
+    int start;     // начальная строка для исключения
+    int step;      // шаг для определения следующей строки
+} Threads;
+
+int gauss_solve(const double *, double* , const double* , int, int);
+
+void *thread_exclusion(void *arg_p);
+
+void matrix_initialization(const double *, const double *);
+
+void direct_move();
+
+void reverse_move(double *);
+
+pthread_barrier_t bar1;
+pthread_barrier_t bar2;
+
+double *gauss_A, *gauss_B, *gauss_X;
+int N, M;
+#endif //MKR_GAUSSMULTITHREAD_H
