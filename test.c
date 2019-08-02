984c4c949b8a30d5419c27aecdbaef0dd9ff75bb
nakmak98 <nakmak1998@gmail.com>
2019-08-02 20:06:26 +0300

    cut test

--- test.c
+++ test.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
 #include <math.h>
 #include "GaussMultithread.h"
 #define N 3
-#define M 2
 double test_square_A[N * N] = {
        1.0, 2.0, 3.0,
        4.0, 5.0, 6.0,
        8.0, 12.0, 14.0
 };
 
-double test_non_square_A[N*M] = {
-        1.0, 2.0, 3.0,
-        4.0, 5.0, 6.0
-};
-
 double test_B[N] = {1.0, 2.0, 3.0};
 
-int check_answer(double *answer){
-   double expected[N] = {0.833333, -1.666667, 1.166667};
+int check_answer(double *answer, const double *expected) {
     for (int i = 0; i < N; ++i) {
          if (isnan(answer[i]))
              return -2;
@@ -30,9 +22,10 @@ int check_answer(double *answer){
 }
 
 int run(){
+    double answer[N] = {0.833333, -1.666667, 1.166667};
     double* X = (double *) calloc(N * N, sizeof(double));
     gauss_solve(test_square_A, X, test_B, N, N);
-    check_answer(X);
+    check_answer(X, answer);
     free(X);
     return 1;
 }
